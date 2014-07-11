3c2d0cebad5800609c7343ea2b9ae0320d72bc2d
Malik Fassi Fihri <fassi.malik@hotmail.com>
2014-07-11 13:56:31 +0200

    Tappable Objects

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var app = {
 		this.store = new LocalStorageStore(function() {
 			$('body').html(new HomeView(self.store).render().el);
 		});
+		registerEvents();
 	},
 	showAlert: function (message, title) {
 		if (navigator.notification) {
@@ -11,7 +12,28 @@ var app = {
 		} else {
 			alert(title ? (title + ": " + message) : message);
 		}
-	}
+	},
+	registerEvents: function() {
+		var self = this;
+    // Check of browser supports touch events...
+    if (document.documentElement.hasOwnProperty('ontouchstart')) {
+        // ... if yes: register touch event listener to change the "selected" state of the item
+        $('body').on('touchstart', 'a', function(event) {
+        	$(event.target).addClass('tappable-active');
+        });
+        $('body').on('touchend', 'a', function(event) {
+        	$(event.target).removeClass('tappable-active');
+        });
+    } else {
+        // ... if not: register mouse events instead
+        $('body').on('mousedown', 'a', function(event) {
+        	$(event.target).addClass('tappable-active');
+        });
+        $('body').on('mouseup', 'a', function(event) {
+        	$(event.target).removeClass('tappable-active');
+        });
+    }
+}
 };
 
 app.initialize();
