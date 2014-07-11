a6e0f7022d3dd3681da906abd6fbda5ea03214e8
Malik Fassi Fihri <fassi.malik@hotmail.com>
2014-07-11 13:35:53 +0200

    Styling

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
 var app = {
-
-	findByName: function() {
-		var self = this;
-		this.store.findByName($('.search-key').val(), function(employees) {
-			$('.employee-list').html(self.employeeLiTpl(employees));
-		});
-	},
 	initialize: function() {
 		var self = this;
 		this.store = new LocalStorageStore(function() {
-			self.renderHomeView();
+			$('body').html(new HomeView(self.store).render().el);
 		});
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-	},
-	renderHomeView: function() {
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
 	},
 	showAlert: function (message, title) {
 		if (navigator.notification) {
